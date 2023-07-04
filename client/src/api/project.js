b06cfd0a5f4c6aceb4e014934205e3d207cb546f
HeoJiye <heoze1031@gmail.com>
2023-07-04 14:30:05 +0900

    front: project, schedule api 추가 (임시)

--- client/src/api/project.js
+++ client/src/api/project.js
@@ -1,24 +1,67 @@
+// 프로젝트 - 출석 DB 어떻게 작성할 건지, 어떻게 구현되는지 정리해서 전달주시면 맞춰서 수정할 예정
+// 추가로 필요한 것
+// 1. user의 point 얻고 잃었던 기록 [{날짜, 내용, 변경된 포인트(+4 / -4), 합계}, ...]
+
+// 의논 필요
+// 1. 출석을 project가 아니라 schedule로 묶는 것도 한번 고려해보기 (더 적절하다 싶은 걸로 진행해주세요) 
+
 export const getProjectList = (search) => {
-    return new Promise((resolve) =>
-      setTimeout(() => {
-        resolve([
-            { 
-                pid: 1,
-                pname: "NPC 정기 회의",
-                type: "팀"
-            },
-            { 
-                pid: 2,
-                pname: "컬러미",
-                type: "팀"
-            },
-            { 
-                pid: 3,
-                pname: "후엠유",
-                type: "팀"
-            },
-        ]);
-      }, 100)
-    );
-  };
-  
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      resolve([
+          { 
+            // 필요 정보를 정리해 둠. 
+            // string or int 형 / 칼럼 통합 or 분리 자유롭게 변형 가능
+            // '개발 완료' 등 값의 종류/이름 등 의논해서 변경 가능
+            pid: 1,
+            pname: "NPC 정기 회의",
+            type: "팀", // 팀 or 개인
+            tname: "[NAME]", // 팀 이름 or 유저 이름
+            process: "개발 중", // 개발 완료, 개발 중
+            period: "2023-07-01 ~ 2023-08-31"
+          },
+          { 
+            pid: 2,
+            pname: "컬러미",
+            type: "팀",
+            tname: "[NAME]",
+            process: "개발 중",
+            period: "2023-07-01 ~ 2023-08-31"
+          },
+          { 
+            pid: 3,
+            pname: "후엠유",
+            type: "팀",
+            tname: "[NAME]",
+            process: "개발 중",
+            period: "2023-07-01 ~ 2023-08-31"
+          },
+      ]);
+    }, 100)
+  );
+};
+
+export const getProjectInfo = (pid) => {
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      resolve({
+        pid: 2,
+        pname: "컬러미",
+        type: "팀",
+        tname: "[NAME]",
+        process: "개발 중",
+        period: "2023-07-01 ~ 2023-08-31",
+        description: "불라불라불라", // 게시글과 같이 markdown 식으로 정리할 까 생각 중
+        member: [1, 2, 3], // userId list
+      });
+    }, 100)
+  );
+}
+
+export const getQuickAttendance = (pid) => {
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      resolve(1); // project에서 현재 생성되어 있는 출석의 id를 가져옴
+    }, 100)
+  );
+}
---
+++ client/src/api/schedule.js
@@ -0,0 +1,54 @@
+// 마찬가지로 상황에 맞춰 변경하시고 통보하시면 맞게 수정해서 쓸게요
+// + date는 string으로 전달함. 원하는 format있으면 맞춰드려요.
+// main 화면에서 할일은 어떻게 할까요
+
+export const getSchedule = (year, month) => {
+    return new Promise((resolve) =>
+        setTimeout(() => {
+        resolve([
+            { 
+                date: "2023-05-01",
+                content: "전체 회의",
+                participants: [1, 2, 3] // 참가자 리스트
+            },
+            { 
+                date: "2023-05-01",
+                content: "전체 회의"
+            },
+            { 
+                date: "2023-05-01",
+                content: "전체 회의"
+            },
+        ]);
+    }, 100)
+  );
+};
+
+export const getNearSchedule = (date) => { // 앞으로 3주 내에 있는 일정들 return (생일 제외)
+    return new Promise((resolve) =>
+        setTimeout(() => {
+        resolve([
+            { 
+                date: "2023-05-01",
+                content: "전체 회의"
+            },
+            { 
+                date: "2023-05-01",
+                content: "전체 회의"
+            },
+            { 
+                date: "2023-05-01",
+                content: "전체 회의"
+            },
+        ]);
+    }, 100)
+  );
+}
+
+export const addSchedule = (schedule) => { // {date, content}
+    return new Promise((resolve) =>
+        setTimeout(() => {
+        resolve(true);
+        }, 100)
+    );
+}

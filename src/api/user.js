50120f9494678333b548ae59bca5dfdb82100ed5
HeoJiye <heoze1031@gmail.com>
2023-09-09 18:39:03 +0900

    feat: login, user api 백엔드와 연결

--- src/api/user.js
+++ src/api/user.js
@@ -1,7 +1,7 @@
-import sha256 from 'crypto-js/sha256';
+import sha256 from "crypto-js/sha256";
 import axios from "./axios";
 
-// 게시글, 댓글 등에서 띄우는 간단한 유저 정보 
+// 게시글, 댓글 등에서 띄우는 간단한 유저 정보
 // (다른 유저의 정보도 필요함 -> uri에 userId도 같이 받기 가람님이 구현한 형태 참고)
 export function readUser(userno) {
   return axios.get("/mypage", {
@@ -26,41 +26,20 @@ export const readUserInfo = (userno) => {
 
 export const login = (loginId, raw_password) => {
   let password = sha256(raw_password).toString();
-  
-  return new Promise((resolve) =>
-    setTimeout(() => {
-      if (loginId === "1234" && password === sha256("1234").toString()) {
-        sessionStorage.setItem("loginSession", 1);
-        resolve(true);
-      } else resolve(false);
-    }, 100)
-  );
+  return axios.post("/login", { userId: loginId, userPw: password }, true);
 };
+
 export const logout = () => {
-  return new Promise((resolve) =>
-    setTimeout(() => {
-      sessionStorage.setItem("loginSession", "");
-      resolve(true);
-    }, 100)
-  );
+  return axios.post("/logout", {}, true);
 };
 
 export const getLoginSession = () => {
-  return new Promise((resolve) =>
-    setTimeout(() => {
-      resolve(sessionStorage.getItem("loginSession"));
-    }, 100)
-  );
+  return axios.get("/login", 1);
 };
 
 export const signup = (loginId, raw_password, nickname, email) => {
   let password = sha256(raw_password).toString();
-
-  return new Promise((resolve) =>
-    setTimeout(() => {
-      resolve(true);
-    }, 100)
-  );
+  return axios.post("/users", { userId: userId, userPw: password, nickname, email }, true);
 };
 
 export const vaildateNickname = (nickname) => {

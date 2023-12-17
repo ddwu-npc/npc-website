52af16122287159ee9b4c7f7d2931b4145e7a03e
karam Choi <riverchoi2523@gmail.com>
2023-12-17 15:06:56 +0900

    게시글 검색 결과 받아옴

--- src/components/board/search.jsx
+++ src/components/board/search.jsx
@@ -1,19 +1,47 @@
 import { createRef, useEffect } from "react";
 import { Icon } from "@iconify/react";
+import { useNavigate } from "react-router-dom";
 import styles from "./style.module.scss";
 
 export default () => {
   const formRef = createRef();
   const submitRef = createRef();
+  const navigate = useNavigate();  // 추가
 
   useEffect(() => {
     submitRef.current.onclick = () => {
-      formRef.current.submit();
+      //formRef.current.submit();
+      HandleSubmit();
     };
   });
+
+  const HandleSubmit = () => {
+    const formData = new FormData(formRef.current);
+    const queryParams = new URLSearchParams(formData).toString();
+
+    console.log("queryParams", queryParams);
+
+    //navigate(`/board/1/search?${queryParams}`);
+    fetch(`/board/1/search`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: queryParams,
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        // Handle the response data as needed
+      })
+      .catch(error => console.error(error));
+
+    // No need to navigate here if the POST request is handled asynchronously
+  };
+
   return (
     <form className={styles.search} ref={formRef}>
-      <select name="range">
+      <select name="rangePost">
         <option value="0">전체</option>
         <option value="1">임원</option>
         <option value="2">팀장</option>

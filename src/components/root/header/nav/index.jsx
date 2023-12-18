c131e1d5c966aa36121f9c49edea9aeec507e781
karam Choi <riverchoi2523@gmail.com>
2023-12-18 19:50:23 +0900

    게시판 이름 수정

--- src/components/root/header/nav/index.jsx
+++ src/components/root/header/nav/index.jsx
@@ -8,8 +8,8 @@ export default (props) => {
     <div className={styles.nav}>
       <div className={styles.taps}>
         <Tap value={"게시판"} dropdown={<BoardListDropdown />} />
-        <Tap link="/board/1" value={"중요 게시판(1)"} />
-        <Tap link="/board/2" value={"중요 게시판(2)"} />
+        <Tap link="/board/1" value={"공지사항"} />
+        <Tap link="/board/2" value={"강의자료"} />
         <Tap link="/project" value={"프로젝트"} />
         <Tap link="/calendar" value={"일정"} />
       </div>

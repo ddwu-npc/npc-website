bb8e292b7b7b392d9bda0d8e1ebe3ffc87431760
Fornamin <97093507+Fornamin@users.noreply.github.com>
2023-12-25 00:52:35 +0900

    add: 프로젝트 수정 데이터 들어가게(수정하면 바로 오류)

--- src/components/project/edit/index.jsx
+++ src/components/project/edit/index.jsx
@@ -14,16 +14,18 @@ import styles from "./style.module.scss";
 
 export const loader = async ({ params }) => {
     if (params.pid) {
-        return await getProjectInfo(params.pid);
+        const data = await getProjectInfo(params.pid);
+        return data;
     } 
     else {
+        const currentDate = new Date().toISOString().slice(0, 10); // YYYY-MM-DD 형식
         return {
             pname: "",
             type: 0,
             tname: "",
             process: "",
-            startDate: "2023-06-06",
-            endDate: "2023-07-07",
+            startDate: currentDate,
+            endDate: currentDate,
             description: "",
             member: []
         };
@@ -45,33 +47,33 @@ export default () => {
                     <input 
                         type="text"
                         name="pname" 
-                        defaultValue={project.pname}
-                        onChange={(e) => setProject({...project, pname: e.target.value})}/>
+                        defaultValue={project.projectRes.pname}
+                        onChange={(e) => setProject({...project.projectRes, pname: e.target.value})}/>
                 </p>
                 <p>
                     <label>진행 상황</label>
                     <select 
                         name="process"
-                        defaultValue={project.process}
-                        onChange={(e) => setProject({...project, process: e.target.value})}>
+                        defaultValue={project.projectRes.process}
+                        onChange={(e) => setProject({...project.projectRes, process: e.target.value})}>
                         <option value={1}>팀</option>
                         <option value={2}>개인</option>
                     </select>
                 </p>
                 <p>
                     <label>프로젝트 기간</label>
-                    <input type="date" name="startDate" defaultValue={project.startDate}
+                    <input type="date" name="startDate" defaultValue={project.projectRes.startDate}
                         max={project.endDate}
-                        onChange={(e) => setProject({...project, startDate: e.target.value})}/>
+                        onChange={(e) => setProject({...project.projectRes, startDate: e.target.value})}/>
                     {" ~ "}
-                    <input type="date" name="endDate" defaultValue={project.endDate}
+                    <input type="date" name="endDate" defaultValue={project.projectRes.endDate}
                         min={project.startDate}
-                        onChange={(e) => setProject({...project, endDate: e.target.value})}/>
+                        onChange={(e) => setProject({...project.projectRes, endDate: e.target.value})}/>
                 </p>
                 <div>
                     <CodeMirror
                         height= "400px"
-                        value={project.description}
+                        value={project.projectRes.description}
                         theme={githubLightInit({
                             settings: {
                                 fontFamily: `"Fira Code", "Fira Mono", Menlo, Consolas, "DejaVu Sans Mono", monospace`,
@@ -80,19 +82,19 @@ export default () => {
                         })}
                         basicSetup={{ highlightActiveLine: false, lineNumbers: false }}
                         extensions={[markdown()]}
-                        onChange={(value, viewUpdate) => setProject({...project, description: value})}
+                        onChange={(value, viewUpdate) => setProject({...project.projectRes, description: value})}
                     />
                 </div>
                 <div className={styles.button}>
                     {project.pid 
                         ? <input type="button" value="수정"
                             onClick={async () => {
-                                if (await updateProject(project)) navigate(`/project/${project.pid}`);
+                                if (await updateProject(project.projectRes)) navigate(`/project/${project.projectRes.pid}`);
                                 else alert("프로젝트 수정에 실패했습니다.\n해당 현상이 반복되면 관리자에게 문의하세요.");
                             }}/>
                         : <input type="button" value="생성"
                             onClick={async () => {
-                                if (await createProject(project)) navigate(`/project`);
+                                if (await createProject(project.projectRes)) navigate(`/project`);
                                 else alert("프로젝트 생성에 실패했습니다.\n해당 현상이 반복되면 관리자에게 문의하세요.");
                             }}/> }
                 </div>

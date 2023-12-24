80276786ddcee4fc18c0bbe876635a0db22bfcea
Fornamin <97093507+Fornamin@users.noreply.github.com>
2023-12-25 00:40:50 +0900

    update: 상세페이징서 부서명 보이게

--- src/components/project/view/index.jsx
+++ src/components/project/view/index.jsx
@@ -8,14 +8,13 @@ import Tag from "components/commons/tag";
 import styles from "./style.module.scss";
 
 import { getProjectInfo, deleteProject, getQuickAttendance } from "api/project";
-import { readUser } from "api/user";
 import { createAttendance } from "api/attendance";
 import { Icon } from "@iconify/react";
 
 export const loader = async ({ params }) => {
     const pid = params.pid;
     const data = await getProjectInfo(pid);
-    
+    console.log(data);
     return data;
 };
 
@@ -24,6 +23,7 @@ export default () => {
     const navigate = useNavigate();
 
     const projectData = useLoaderData();
+    console.log(projectData);
     const [option, setOption] = useState(false);
 
     return (
@@ -31,8 +31,8 @@ export default () => {
         <Header text="프로젝트" src="/project"/>
         <div className={styles.view}>
            <div className={styles.title}>
-                <Tag text={projectData.project.process} color="#FED5A5"/>
-                <h2>{projectData.project.pname}</h2>
+                <Tag text={projectData.projectRes.process} color="#FED5A5"/>
+                <h2>{projectData.projectRes.pname}</h2>
                 <div className={styles.option}>
                     <Icon 
                         icon="bi:three-dots-vertical" color="#7E7E7E" 
@@ -40,7 +40,7 @@ export default () => {
                     {option && (
                         <div>
                             <div onClick={async () => {
-                                const result = await createAttendance(projectData.project.pid);
+                                const result = await createAttendance(projectData.projectRes.pid);
                                 if(result) {
                                     navigate(`/attendance/${result}`);
                                 }
@@ -50,7 +50,7 @@ export default () => {
                             }}>출석 생성</div>
                             <div onClick={() => navigate("edit")}>수정</div>
                             <div onClick={async () => {
-                                if (await deleteProject(projectData.project.pid)) navigate("/project");
+                                if (await deleteProject(projectData.projectRes.pid)) navigate("/project");
                                 else {
                                     alert("프로젝트 삭제에 실패했습니다.\n해당 현상이 반복될 시 관리자에게 문의바랍니다.")
                                 }
@@ -63,34 +63,47 @@ export default () => {
                 <div className={styles.describtion}>
                     <div>
                         <label>장르</label>
-                        <span>{projectData.project.type}</span>
+                        <span>{projectData.projectRes.type}</span>
                     </div>
                     <div>
                         <label>팀장</label>
-                        <span>{projectData.project.leader}</span>
+                        <span>{projectData.projectRes.leader}</span>
                     </div>
                     <div>
                         <label>개발 기간</label>
-                        <span>{projectData.project.startDate}~{projectData.project.endDate}</span>
+                        <span>{projectData.projectRes.startDate}~{projectData.projectRes.endDate}</span>
                     </div>
                     <div>
                         <label>팀원</label>
                         <span>
-                            {projectData.userList.map((user, index) => (
-                                <span key={index}>{user}</span>
-                            ))}
-                        </span>
+                        {Object.entries(projectData.userList).map(([name, department], index) => (
+                            <span
+                                key={index}
+                                className={
+                                    department === "개발팀"
+                                        ? "dev-team"
+                                        : department === "디자인팀"
+                                        ? "design-team"
+                                        : department === "기획팀"
+                                        ? "planning-team"
+                                        : ""
+                                }
+                            >
+                                {name} - {department}
+                            </span>
+                        ))}
+                    </span>
                     </div>
                     <div>
                         <label>프로젝트 설명</label>
-                        <div>{projectData.project.content}</div>
+                        <div>{projectData.projectRes.content}</div>
                     </div>
                 </div>
                 <img/>
             </div>
             <div className={styles.button}>
                 <input type="button" value="빠른 출석 바로가기" onClick={async () => {
-                   navigate(`/attendance/${await getQuickAttendance(projectData.project.pid)}`); 
+                   navigate(`/attendance/${await getQuickAttendance(projectData.projectRes.pid)}`); 
                 }}/> 
             </div>
         </div>

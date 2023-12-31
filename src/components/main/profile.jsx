b546c7d6692f0d53ce87ffb0f90a5876b37c01c3
dokyung-kang <kangdk02@naver.com>
2023-12-31 16:58:57 +0900

    update: 메인 페이지 프로필 사진

--- src/components/main/profile.jsx
+++ src/components/main/profile.jsx
@@ -1,17 +1,39 @@
 import { Link, useLoaderData } from "react-router-dom";
 import { Icon } from "@iconify/react";
+import { useState, useEffect } from "react";
 
 import styles from "./style.module.scss";
+import { readUserFile, getUserno } from "api/user"; 
 
 export default () => {
-    const { user } = useLoaderData();
+    const { user } = useLoaderData();    
+    const [attachment, setAttachment] = useState([]);
+
+    useEffect(() => {
+        const fetchAttachment = async () => {
+          try {
+            const userno = await getUserno();
+            const result = await readUserFile(userno); 
+            setAttachment(result);
+          } catch (error) {
+            console.error("Error fetching user file:", error);
+          }
+        };
+      
+        fetchAttachment();
+      }, []);
+
 
     return (
         <div className={`${styles.box} ${styles.profile}`}>
             <div className={styles.link}>
                 <Link to="/mypage">마이페이지 <Icon icon="icon-park-outline:right" color="#B1B1B1"/></Link>
             </div>
-            <img src={user.profile} />
+            {attachment && attachment.sName ? (
+            <img src={`/userfile/look/${attachment.sName}`} />
+            ) : (
+              <img src={`/userfile/look/default.png`} />
+            )}
             <div className={styles.content}>
                 <div><Icon icon="charm:person" /> 닉네임: {user.nickname}</div>
                 <div><Icon icon="carbon:condition-point" /> NPC Point: {user.npcPoint}</div>

3132e132afd0d973568ac7be0b2ac8cc03cb7974
Fornamin <97093507+Fornamin@users.noreply.github.com>
2024-01-05 00:29:09 +0900

    update: 페이징 완료

--- src/components/project/table/index.jsx
+++ src/components/project/table/index.jsx
@@ -1,19 +1,33 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useLoaderData } from "react-router-dom";
 import styles from "./style.module.scss";
-
 import Header from "./header";
 import Project from "./project";
 import Nav from "./nav";
+import { getProjectList } from "api/project"; 
 
 export default (props) => {
   const { projectPaging } = useLoaderData();
   const [page, setPage] = useState(1);
+  const [curProjectList, setCurProjectList] = useState(projectPaging.projects);
+  const [pageInfo, setPageInfo] = useState(projectPaging.pageInfo);
 
-  const curProjectList = projectPaging.projects;
-  const pageInfo = projectPaging.pageInfo;
+  useEffect(() => {
+    const fetchData = async () => {
+      const updatedProjectPaging = await getProjectList({
+        range: "", 
+        searchRange: "", 
+        text: "", 
+      }, page);
+
+      setCurProjectList(updatedProjectPaging.projects);
+      setPageInfo(updatedProjectPaging.pageInfo);
+    };
+
+    fetchData();
+  }, [page]); // 
+
   const emptyProjects = [];
-
   while (curProjectList.length + emptyProjects.length < 11) {
     emptyProjects.push(
       <div key={`project_table_empty_${emptyProjects.length}`}>
@@ -33,11 +47,7 @@ export default (props) => {
         ))}
         {emptyProjects}
       </div>
-      <Nav
-        cur={pageInfo[0]}
-        max={pageInfo[1]}
-        setPage={setPage}
-      />
+      <Nav cur={pageInfo[0]} max={pageInfo[1]} setPage={setPage} />
     </div>
   );
 };

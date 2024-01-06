6c5431b2b871fed51bf07adbc016f5e3ae3a6688
karam Choi <riverchoi2523@gmail.com>
2024-01-06 22:20:04 +0900

    댓글 프로필 추가

--- src/components/board/post/read/comments/comment.jsx
+++ src/components/board/post/read/comments/comment.jsx
@@ -1,13 +1,35 @@
 import styles from "./style.module.scss";
+import { useState, useEffect } from "react";
 
 import Option from "./option";
+import { readUserFile } from "api/user"; 
 
 export default ({ comment }) => {
+  const [attachment, setAttachment] = useState([]);
+  useEffect(() => {
+    if(comment.user.profile==1){
+      const fetchAttachment = async () => {
+        try {
+          const userno = comment.user.userNo;
+          const result = await readUserFile(userno); 
+          setAttachment(result);
+        } catch (error) {
+          console.error("Error fetching user file:", error);
+        }
+      };
+      fetchAttachment();
+    }
+  }, []);
+  
   return (
     <>
       <div className={styles.comment}>
         <Option commentId={comment.commentId}/>
-        <img src={comment.user.profile} />
+        {attachment && comment.user.profile==1 ? (
+            <img src={`/userfile/look/${attachment.sName}`} />
+            ) : (
+              <img src={`/userfile/look/default.png`} />
+        )}
         <div>
           <div className={styles.info}>
             <span className={styles.writer}>{comment.user.nickname}</span>
